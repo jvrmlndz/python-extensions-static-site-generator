a0bf295cb889e00af0c432514714fec58b849304
jvrmlndz <melendezdroz@gmail.com>
2022-06-13 11:33:22 -0400

    .

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -2,5 +2,6 @@ from ssg import hooks, parsers
 
 files = []
 
+@hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    pass
+    valid = lambda p: not isinstance(p, parsers.ResourceParser) 
