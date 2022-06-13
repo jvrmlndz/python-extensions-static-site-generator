b1548fcc14e8f804e16d50856cf2fd78f3f7a590
jvrmlndz <melendezdroz@gmail.com>
2022-06-13 10:52:49 -0400

    .

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -7,4 +7,11 @@ def register(hook, order=0):
     
     return register_callback
 
+def event(hook, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
+
+
+
 
